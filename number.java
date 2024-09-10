ec542e06c9b1874d780678e22d6bfbed19db522e
Ubuntu <ubuntu@ip-172-31-27-7.us-east-2.compute.internal>
2024-09-10 02:19:39 +0000

    code added in dev

--- number.java
+++ number.java
@@ -1,1 +1,13 @@
-welcome-test for merg- testxxxxxx
+Public class login
+{
+	public static void main()
+	{
+		int i=0;
+		for (i=0;i<==45;i++
+		{
+			system.out printIn ("Number: "+i);
+		}
+	}
+}
+
+
